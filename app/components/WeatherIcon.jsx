0587af9002731a4f98b274dfaa6bc14bab1e56a8
Steven Harbaruk <steven.harbaruk@gmail.com>
2017-05-24 23:58:37 -0700

    updated Icon components to display a status

--- app/components/WeatherIcon.jsx
+++ app/components/WeatherIcon.jsx
@@ -8,75 +8,75 @@ var WeatherIcon = React.createClass({
       switch (details.icon) {
           case '01d':
             return (
-              <h3>Test</h3>
+              <h3>Clear Sky</h3>
             );
           case '01n':
             return (
-              <h3>DATA IS LIVE</h3>
+              <h3>Clear Sky (Night)</h3>
             );
           case '02d':
             return (
-              <h3>Should be here</h3>
+              <h3>Few Clouds</h3>
             );
             case '02n':
               return (
-                <h3>DATA IS LIVE</h3>
+                <h3>Few Clouds (Night)</h3>
               );
             case '03d':
               return (
-                <h3>Should be here</h3>
+                <h3>Scattered Clouds</h3>
               );
               case '03n':
                 return (
-                  <h3>DATA IS LIVE</h3>
+                  <h3>Scattered Clouds (Night)</h3>
                 );
               case '04d':
                 return (
-                  <h3>Should be here</h3>
+                  <h3>Broken Clouds</h3>
                 );
                 case '04n':
                   return (
-                    <h3>DATA IS LIVE</h3>
+                    <h3>Broken Clouds (Night)</h3>
                   );
                 case '09d':
                   return (
-                    <h3>Should be here</h3>
+                    <h3>Rain Showers</h3>
                   );
                   case '09n':
                     return (
-                      <h3>DATA IS LIVE</h3>
+                      <h3>Rain Showers (Night)</h3>
                     );
                   case '10d':
                     return (
-                      <h3>Should be here</h3>
+                      <h3>Rain</h3>
                     );
                     case '10n':
                       return (
-                        <h3>Should be here</h3>
+                        <h3>Rain (Night)</h3>
                       );
                       case '11d':
                         return (
-                          <h3>Should be here</h3>
+                          <h3>Thunderstorm</h3>
                         );
                         case '11n':
                           return (
-                            <h3>Should be here</h3>
+                            <h3>Thunderstorm (Night)</h3>
                           );
                           case '13d':
                             return (
-                              <h3>Should be here</h3>
+                              <h3>Snow</h3>
                             );
                             case '13n':
                               return (
-                                <h3>Should be here</h3>
+                                <h3>Snow (Night)</h3>
                               );
                               case '50d':
                                 return (
-                                  <h3>Should be here</h3>
+                                  <h3>Mist</h3>
                                 );
                                 case '50n':
                                   return (
-                                    <h3>Should be here</h3>
+                                    <h3>Mist (Night)</h3>
                                   );
           // Add more icons here
         }
